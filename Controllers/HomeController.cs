3ba2812c79f87b53589de858a6c36ed3de061613
aslihilaldemir <aslihilaldemir@gmail.com>
2021-05-26 21:38:23 +0300

    Initial check-in of module CetTodoWeb

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using CetTodoWeb.Data;
+using CetTodoWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetTodoWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext dbContext;
+
+        public HomeController(ILogger<HomeController> logger,ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            var query = dbContext.TodoItems.Include(t=>t.Category).Where(t => !t.IsCompleted).OrderBy(t => t.DueDate).Take(3);
+
+
+            List<TodoItem> result =await query.ToListAsync();
+
+
+            return View(result);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
